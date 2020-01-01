9b0ca0a57d62b2e013ac12c074bc08b1486eea74
Tim Shamu <root@Timothys-MacBook-Air.local>
2020-01-01 21:56:20 +0000

    Added Collections

--- Mexico/src/com/sinyo/Main.java
+++ Mexico/src/com/sinyo/Main.java
@@ -1,9 +1,21 @@
 package com.sinyo;
 
+
+import com.sinyo.collections.Customer;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
-	// write your code here
+        List<Customer> customers = new ArrayList<>();
+        customers.add(new Customer("Zephyr"));
+        customers.add(new Customer("WRashpiya"));
+        customers.add(new Customer("Stockley"));
+        Collections.sort(customers);
+        System.out.println(customers);
+
     }
 }
